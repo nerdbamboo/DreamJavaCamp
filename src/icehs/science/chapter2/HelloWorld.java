77ea01aba460e431c1f14ea34a48ca05871fc984
학생52 <52@DESKTOP-6H8ITL8>
2019-05-05 14:15:30 +0900

    f

--- src/icehs/science/chapter2/HelloWorld.java
+++ src/icehs/science/chapter2/HelloWorld.java
@@ -4,7 +4,12 @@ package icehs.science.chapter2;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld"); // MyFirstJava Project
+		//진호 다리 복한이
+		/*
+		 * My Fist Java Class
+		 */
+		
 		System.out.println("꿈의학교");
 		
 
