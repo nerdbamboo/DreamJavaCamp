86e8476f79289ea6e06ad95912e60a0a70291f80
학생52 <52@DESKTOP-6H8ITL8>
2019-05-05 14:16:12 +0900

    f word

--- src/icehs/science/chapter2/HelloWorld.java
+++ src/icehs/science/chapter2/HelloWorld.java
@@ -5,11 +5,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld"); // MyFirstJava Project
-		//진호 다리 복한이
-		/*
-		 * My Fist Java Class
-		 */
-		
 		System.out.println("꿈의학교");
 		
 
