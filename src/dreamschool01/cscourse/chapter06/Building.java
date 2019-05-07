b51297dfea65156484a4f4843d8acb26dbc79f5d
학생52 <52@DESKTOP-6H8ITL8>
2019-05-07 11:32:55 +0900

    HeyBoy

--- src/dreamschool01/cscourse/chapter06/Building.java
+++ src/dreamschool01/cscourse/chapter06/Building.java
@@ -1,5 +1,15 @@
 package dreamschool01.cscourse.chapter06;
 
 public class Building {
+	String name;
+	String address;
+	int totalFloor;
+	
+	public void moveElievator() {
+		System.out.println("郡府海捞磐 款青");
+	}
+	public void chageAddress(String newAddress) {
+		address = newAddress;
+	}
 
 }
--- src/dreamschool01/cscourse/chapter06/Student.java
+++ src/dreamschool01/cscourse/chapter06/Student.java
@@ -1,5 +1,29 @@
 package dreamschool01.cscourse.chapter06;
 
 public class Student {
+	String name;
+	String studentNum;
+	double korean;
+	double english;
+	double math;
+	
+	double average() {
+		double avg = (korean + english + math) / (double)3;
+		
+		return avg;
+	}
+	
+	void printStudendtInfo() {
+		System.out.println("일름  :" + name);
+		System.out.println("학번 : " + studentNum);
+		System.out.println("국어 점수  :" + korean);
+		System.out.println("영어 점수 : " + english);
+		System.out.println("수학 점수 : " + math);
+		System.out.println("평균 점수 : " + average());
+	}
+	void chageSutdentID(String newId) {
+		studentNum = newId;
+	}
+	
 
 }
